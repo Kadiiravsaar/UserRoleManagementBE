fc8fe806b453b0b50972847fe0d884b2479daae7
kadiiravsaar <kadiiravsar@gmail.com>
2024-06-10 04:16:58 +0300

    refactor

--- URM.Service/Rules/AuthBusinessRules.cs
+++ URM.Service/Rules/AuthBusinessRules.cs
@@ -10,13 +10,37 @@ public class AuthBusinessRules
 {
 	private readonly UserManager<AppUser> _userManager;
 	private readonly RoleManager<AppRole> _roleManager;
+	private readonly SignInManager<AppUser> _signInManager;
 
-	public AuthBusinessRules(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+	public AuthBusinessRules(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
 	{
 		_userManager = userManager;
 		_roleManager = roleManager;
+		_signInManager = signInManager;
 	}
 
+	/// <summary>
+	/// İş Kuralı: Şifre kontrolü
+	/// </summary>
+	/// <param name="user"></param>
+	/// <param name="password"></param>
+	/// <returns></returns>
+	/// <exception cref="BusinessException"></exception>
+	public async Task CheckPasswordAsync(AppUser user, string password)
+	{
+		var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+		if (!result.Succeeded)
+		{
+			throw new BusinessException(Messages.InvalidUsernameOrPassword); 
+		}
+	}
+
+	/// <summary>
+	///  İş Kuralı: Rol varlığını kontrol et ve gerekiyorsa oluştur
+	/// </summary>
+	/// <param name="roleName"></param>
+	/// <returns></returns>
+	/// <exception cref="InvalidOperationException"></exception>
 	public async Task EnsureRoleExists(string roleName)
 	{
 		var roleExists = await _roleManager.RoleExistsAsync(roleName);
--- URM.Service/Services/AuthService.cs
+++ URM.Service/Services/AuthService.cs
@@ -15,35 +15,31 @@ namespace URM.Service.Services
 	public class AuthService : IAuthService
 	{
 		private readonly UserManager<AppUser> _userManager;
-		private readonly SignInManager<AppUser> _signInManager;
+
 		private readonly IMapper _mapper;
 		private readonly ITokenService _tokenService;
 		private readonly AuthBusinessRules _authBusinessRules;
 
 
 
-		public AuthService(UserManager<AppUser> userManager, IMapper mapper, ITokenService tokenService, AuthBusinessRules authBusinessRules, SignInManager<AppUser> signInManager)
+		public AuthService(UserManager<AppUser> userManager, IMapper mapper, ITokenService tokenService, AuthBusinessRules authBusinessRules)
 		{
 			_userManager = userManager;
 			_mapper = mapper;
 			_tokenService = tokenService;
 			_authBusinessRules = authBusinessRules;
-			_signInManager = signInManager;
 		}
 
 		public async Task<NewUserDto> SignIn(LoginDto loginDto)
 		{
-
-			loginDto.EnsureNotNull(Messages.FillAllFields); 
+			loginDto.EnsureNotNull(Messages.FillAllFields);
 
 			var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.UserName.ToLower());
+
 			user.EnsureNotNull(Messages.UserNotFound);
 
-			var passwordCheckResult = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-			if (!passwordCheckResult.Succeeded)
-			{
-				throw new BusinessException(Messages.InvalidUsernameOrPassword); // Daha anlamlı hata yönetimi
-			}
+
+			await _authBusinessRules.CheckPasswordAsync(user, loginDto.Password);
 
 			var userInfo = _mapper.Map<NewUserDto>(user);
 			userInfo.Token = await _tokenService.CreateToken(user);
@@ -53,14 +49,15 @@ namespace URM.Service.Services
 
 		public async Task<NewUserDto> SignUp(RegisterDto registerDto)
 		{
-			registerDto.EnsureNotNull(Messages.FillAllFields); 
+			registerDto.EnsureNotNull(Messages.FillAllFields);
 
 			var user = _mapper.Map<AppUser>(registerDto);
 			var result = await _userManager.CreateAsync(user, registerDto.Password);
 
 			if (result.Succeeded)
 			{
-				await _authBusinessRules.EnsureRoleExists(Roles.User.ToString()); 
+
+				await _authBusinessRules.EnsureRoleExists(Roles.User.ToString());
 				await _userManager.AddToRoleAsync(user, Roles.User.ToString());
 
 				var token = await _tokenService.CreateToken(user);
